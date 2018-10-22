15acbe768148da9415254d9ad68ce3006e534641
SomewhatNihilist <arrdude@gmail.com>
2018-10-22 01:51:18 -0300

    Agregado test basicos de funcionalidad

--- Tarea2/main.c
+++ Tarea2/main.c
@@ -7,13 +7,79 @@
 #include "genetico.h"
 #include <time.h> // Para generar un seed aleatorio
 
+void printNodo(tNodo node);
+
 int main(int argc, char const *argv[]) {
+	int testi = 8; char testc = 'D'; char testb = 0; tNodo nodo;
+	srand(0);
 	void *a = generarSolucion(5);
 	void *b = generarSolucion(5);
-	imprimirSolucion(a); imprimirSolucion(b);
+
+	//Tests basicos
+	printf("a: "); imprimirSolucion(a); //(E, c) (5, i) (1, b) (A, c) (1, i)
+	printf("b: "); imprimirSolucion(b); //(1, b) (1, b) (0, b) (6, i) (E, c)
+	printf("Movement: %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); //5 (!, x) //imprimirSolucion no resetea la pocicion cuando termina.
+	l_moveToStart((tLista*)a); printNodo(l_getValue((tLista*)a)); //(E, c)
+	l_moveToPos((tLista*)a, 3); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 3 (A, c)
+	l_moveToEnd((tLista*)a); printNodo(l_getValue((tLista*)a)); //(1, i)
+	l_moveToPos((tLista*)a, 1); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 1 (5, i)
+	//  Test remove
+	printf("\nRemove       : "); nodo = l_remove((tLista*)a); printNodo(nodo); // (5, i)
+	printf(" pos:%d, size:%d ", ((tLista*)a)->pos, ((tLista*)a)->size); printNodo(l_getValue((tLista*)a)); // pos:1, size:4 (1, b)
+	printf("\n    result: ");imprimirSolucion(a); //(E, c) (1, b) (A, c) (1, i)
+	printf("  remove last: "); l_moveToEnd((tLista*)a); nodo = l_remove((tLista*)a); printNodo(nodo); //(1, i)
+	printf(" pos:%d, size:%d ", ((tLista*)a)->pos, ((tLista*)a)->size); printNodo(l_getValue((tLista*)a)); // pos:3, size:3 (!, x)
+	printf("\n    result: ");imprimirSolucion(a); //(E, c) (1, b) (A, c)
+	//  Test insert
+	l_moveToPos((tLista*)a, 1);
+	printf("\nInsert: "); l_insert((tLista*)a, 'i', &testi); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 1 (8, i)
+	l_next((tLista*)a); l_next((tLista*)a); l_insert((tLista*)a, 'c', &testc); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 3 (D, c)
+	l_moveToEnd((tLista*)a); l_next((tLista*)a); l_insert((tLista*)a, 'b', &testb); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 5 (0, b)
+	l_moveToStart((tLista*)a); l_insert((tLista*)a, 'b', &testb); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 0 (0, b)
+	printf("\n    a: "); imprimirSolucion(a); //(0, b) (E, c) (8, i) (1, b) (D, c) (A, c) (0, b)
+	//Test de copia
+	borrar(a); a = copiar(b);
+	printf("\na = copia(b): (a)%p %c= (b)%p\n", a, a==b?'=':'!', b); printf("a: "); imprimirSolucion(a);
+	borrar(a); borrar(b);
+
+
+	printf("\nSize 10:\n");
+	srand(0);
+	a = generarSolucion(10);
+	b = generarSolucion(10);
+	//Tests basicos
+	printf("a: "); imprimirSolucion(a); //(E, c) (5, i) (1, b) (A, c) (1, i)
+	printf("b: "); imprimirSolucion(b); //(1, b) (1, b) (0, b) (6, i) (E, c)
+	printNodo(l_getValue((tLista*)a)); //nulls (!, x) //imprimirSolucion no resetea la pocicion cuando termina.
+	l_moveToStart((tLista*)a); printNodo(l_getValue((tLista*)a)); //(E, c)
+	l_moveToPos((tLista*)a, 3); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 3 (A, c)
+	l_moveToEnd((tLista*)a); printNodo(l_getValue((tLista*)a)); //(1, i)
+	l_moveToPos((tLista*)a, 1); printf(" %d ", ((tLista*)a)->pos); printNodo(l_getValue((tLista*)a)); // 1 (5, i)
+	//Test de copia
+	borrar(a); a = copiar(b);
+	printf("\na = copia(b): (a)%p %c= (b)%p\n", a, a==b?'=':'!', b); printf("a: "); imprimirSolucion(a);
+	borrar(a); borrar(b);
+
+
+	printf("\nRandoms:\n");
 	srand(time(NULL));
 	a = generarSolucion(5);
 	b = generarSolucion(5);
+
 	imprimirSolucion(a); imprimirSolucion(b);
+	borrar(a); a = copiar(b);
+	printf("Copia:\n"); imprimirSolucion(a);
+	borrar(a); borrar(b);
+
 	return EXIT_SUCCESS;
+}
+
+
+void printNodo(tNodo node) {
+	switch (node.tipo) {
+		case 'i': printf("(%d, %c)", *(int*)node.dato, node.tipo); break;
+		case 'c': printf("(%c, %c)", *(char*)node.dato, node.tipo); break;
+		case 'b': printf("(%hhd, %c)", *(int*)node.dato, node.tipo); break;
+		default: printf("(!, %c)", node.tipo); break;
+	}
 }
