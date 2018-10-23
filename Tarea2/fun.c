97aedf1e5cbb6bcc68b428810e475401c89c9c25
SomewhatNihilist <arrdude@gmail.com>
2018-10-23 00:46:16 -0300

    Ahora hace algo deberia prioritizar tipos 'b','c','i' en ese orden

--- Tarea2/fun.c
+++ Tarea2/fun.c
@@ -6,5 +6,10 @@
 
 int fun(void* nodo) {
 	tNodo*n = (tNodo*)nodo;
-	return rand();
+	switch (n->tipo) {
+		case 'i': return *(int*)n->dato;
+		case 'c': return *(char*)n->dato - 'A' + 10;
+		case 'b': return *(char*)n->dato+17;
+		default: return 0;
+	} //En teoria, termina con numeros altos, donde 'i'<'c'<'b'
 }
