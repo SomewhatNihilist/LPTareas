8c006bcdac13d5d2c2f83db2f18f64e00e6da728
SomewhatNihilist <arrdude@gmail.com>
2018-11-28 22:03:56 -0300

    Problema6 funciona

--- Tarea4/Problema6.rkt
+++ Tarea4/Problema6.rkt
@@ -7,22 +7,29 @@ Parametros:
 lista1 
 lista2
 n entero
-Retorno: Retorna 2 listas que han sido cruzadas desde el primer trozo n
+Retorno: Retorna una lista con las 2 listas que han sido cruzadas desde el primer trozo n
 |#
-(define cruzar
-  (lambda (lista1 lista2 n)
-    (let var ((S1 '()) (S2 '()) (ls1 lista1) (ls2 lista2))
-      (cond
-        ((null? ls1) (cons S2 (list S1)))
-        (else
-         (if (<= (length ls1) n)
-             (var (append S1 (list (car ls2))) (append S2 (list (car ls1))) (cdr ls1) (cdr ls2))
-             (var (append S1 (list (car ls1))) (append S2 (list (car ls2))) (cdr ls1) (cdr ls2))
-             )
-         )
-        )
-      )
-    )
-  )
+(define (cruzar lista1 lista2 n)
+  (if (<= (length lista1) n) (list lista2 lista1) ; Edge-case optimisation
+      (let cutl ((S1 '()) (S2 '()) (ls1 lista1) (ls2 lista2))
+        (if (>= (length S1) n) (list (append (reverse S2) ls1) (append (reverse S1) ls2))
+            (cutl (cons (car ls1) S1) (cons (car ls2) S2) (cdr ls1) (cdr ls2))))))
+#|
+(define (cruzar2 lista1 lista2 n)
+  (if (<= (length lista1) n) (list lista2 lista1) ; Edge-case optimisation
+      (let var ((S1 '()) (S2 '()) (ls1 lista1) (ls2 lista2))
+        (cond
+          ((null? ls1) (list S2 S1))
+          ((<= (length ls1) n) (var (append S1 (list (car ls2))) (append S2 (list (car ls1))) (cdr ls1) (cdr ls2)))
+          (else                (var (append S1 (list (car ls1))) (append S2 (list (car ls2))) (cdr ls1) (cdr ls2)))
+          ))))|#
 ;;Test
+#|
+(cruzar '( a b a a b c) '(a c c b c a) 1)
+(cruzar '( a b a a b c) '(a c c b c a) 2)
 (cruzar '( a b a a b c) '(a c c b c a) 3)
+(cruzar '( a b a a b c) '(a c c b c a) 4)
+(cruzar '( a b a a b c) '(a c c b c a) 5)
+(cruzar '( a b a a b c) '(a c c b c a) 6)
+(cruzar '( a b a a b c) '(a c c b c a) 7)
+|#
