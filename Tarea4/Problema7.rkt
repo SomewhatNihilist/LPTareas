51eb6acbffa6bfe22dfe5c1431cd4a4a42bff8cb
SomewhatNihilist <arrdude@gmail.com>
2018-11-28 23:26:00 -0300

    Ptob7 arreglado

--- Tarea4/Problema7.rkt
+++ Tarea4/Problema7.rkt
@@ -1,20 +1,9 @@
 #lang scheme
 ; Problema 7, Operaciones entre binarios
 
-(define (bin->bool b) (if (or (= b 0) (= b 1))
-                          (if (zero? b) #f #t)
-                          (raise-argument-error 'bin->bool
-                                                "0 or 1"
-                                                b)))
-(define (bool->bin b) (if b 1 0))
+(define (operar func) (lambda(lista1 lista2) (map func lista1 lista2)))
 
-(define (operar foo lst1 lst2)
-  (if (= (length lst1) (length lst2))
-      (map (lambda (a b) (bool->bin (foo (bin->bool a) (bin->bool b)))) lst1 lst2)
-      (raise-argument-error 'operar
-                            "both lists must have same size"
-                            (format "lst1: ~a, lst2: ~a" (length lst1) (length lst2)))))
-
-
-(operar xor (list 0 1 1 1) (list 0 0 1 0))
+;; Test
+(define (xor a b) (if (not (= a b)) 1 0))
+((operar xor) (list 0 1 1 1) (list 0 0 1 0))
 
